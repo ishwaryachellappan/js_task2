2102b20862b82f042c5274eb73bf6a6309ae0c37
Ishwarya <ishwaryathendral@gmail.com>
2020-08-22 20:36:24 +0530

    [IMP]

--- array.js
+++ array.js
@@ -1,21 +1,29 @@
 var inputArray = [];
 var size = 5; //Maximum Array size
 
-for(var i=0; i<size; i++) {
+
+var inputArray = [];
+var size = 5; //Maximum Array size
+
+var i=0;
+while(i<size) {
 	
 	//Taking Input from user
 	inputArray[i] = prompt('Enter Element ' + (i+1));
+  i++;
 }
 
 for ( var i = 0; i < inputArray.length;i++)
    {
       inputArray[i] = inputArray[i]*inputArray[i]*inputArray[i];
    } 
+   
     
   
     var product= inputArray.reduce(function(a, b){
         return a * b;
-    }, 1);{
+    }, 1);
+    {
   
     if("inputArray=! 0"){
 console.log(product);
