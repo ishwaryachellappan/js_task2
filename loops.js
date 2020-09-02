2950744f5efef308401ef8d1e73d1f160c4aecfd
Ishwarya <ishwaryathendral@gmail.com>
2020-09-02 17:45:38 +0530

    [IMP]

--- loops.js
+++ loops.js
@@ -1,14 +1,20 @@
-function insertDash(num) {
-    let withDash = '';
-    for (let i = 0; i < num.length; i++) {
+const num=prompt();  //025468
+const str = num.toString();
+const result = [str[0]];
 
-        if (num[i] % 2 === 0 && num[i + 1] % 2 === 0) {
-            withDash += `${num[i]}-`;
-        } else {
-            withDash += num[i];
-        }
-
-    }
-    return withDash;
+function call(string){
+for(let x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x]);
+     }
+    else
+     {
+      result.push(str[x]);
+     }
+  }
+return result;
 }
-console.log(insertDash('025468'));
+var answer = call();
+document.write(answer.join(''));
